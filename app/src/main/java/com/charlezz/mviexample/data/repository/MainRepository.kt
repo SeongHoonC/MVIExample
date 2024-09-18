59bf7b77b2e86efbcac48779439002dbb35e6746
SeongHoonC <vrexpert2302@gmail.com>
2024-09-18 18:07:06 +0900

    저장소는 매번 하나씩 증가시킨 목록을 반환한다

--- app/src/main/java/com/charlezz/mviexample/data/repository/MainRepository.kt
+++ app/src/main/java/com/charlezz/mviexample/data/repository/MainRepository.kt
@@ -2,13 +2,20 @@ package com.charlezz.mviexample.data.repository
 
 import com.charlezz.mviexample.data.api.ApiHelper
 import com.charlezz.mviexample.data.model.User
+import kotlinx.coroutines.delay
 import javax.inject.Inject
 
 
 class MainRepository @Inject constructor(
     private val apiHelper: ApiHelper
 ) {
+    private var users = mutableListOf<User>()
+    private var id = 0
 
-    suspend fun getUsers() = List(5) { User(it + 1, "유저$it", "이메일$it") }
-
+    suspend fun getUsers(): List<User> {
+        delay(300L)
+        id++
+        users.add(User(id + 1, "유저$id", "이메일$id"))
+        return users.toList()
+    }
 }
