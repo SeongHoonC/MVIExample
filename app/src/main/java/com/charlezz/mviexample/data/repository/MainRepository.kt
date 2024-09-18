306bab1e1deb7f627ad27b590a3d45303c0ffb23
SeongHoonC <vrexpert2302@gmail.com>
2024-09-18 17:20:11 +0900

    User 는 가짜 값을 반환한다

--- app/src/main/java/com/charlezz/mviexample/data/repository/MainRepository.kt
+++ app/src/main/java/com/charlezz/mviexample/data/repository/MainRepository.kt
@@ -1,6 +1,7 @@
 package com.charlezz.mviexample.data.repository
 
 import com.charlezz.mviexample.data.api.ApiHelper
+import com.charlezz.mviexample.data.model.User
 import javax.inject.Inject
 
 
@@ -8,6 +9,6 @@ class MainRepository @Inject constructor(
     private val apiHelper: ApiHelper
 ) {
 
-    suspend fun getUsers() = apiHelper.getUsers()
+    suspend fun getUsers() = List(5) { User(it + 1, "유저$it", "이메일$it") }
 
 }
